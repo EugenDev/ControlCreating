2392cc1f548f198e98513c6e74437446c19b616f
EugenDev <moaisdean@gmail.com>
2014-12-24 23:52:40 +0500

    Color Picker Done!

--- app/src/main/java/eugeny/n7/controlcreating/MainActivity.java
+++ app/src/main/java/eugeny/n7/controlcreating/MainActivity.java
@@ -1,33 +1,39 @@
 package eugeny.n7.controlcreating;
 
+import android.graphics.Color;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.SurfaceView;
+import android.view.View;
+import android.widget.Button;
 
 
-public class MainActivity extends ActionBarActivity implements MyColorPickerView.OnColorChangedListener {
+public class MainActivity extends ActionBarActivity implements ColorPickerView.OnColorChangedListener, Button.OnClickListener {
 
     private final String TAG = "MAIN_ACTIVITY";
 
+    private Button btn;
     private SurfaceView surfaceView;
-    private MyColorPickerView colorPicker;
+    private ColorPickerView colorPicker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         surfaceView = (SurfaceView)findViewById(R.id.surfaceView);
-        colorPicker = (MyColorPickerView)findViewById(R.id.colorPicker);
+        colorPicker = (ColorPickerView)findViewById(R.id.colorPicker);
         colorPicker.setOnColorChangedListener(this);
+        btn = (Button)findViewById(R.id.button);
+        btn.setOnClickListener(this);
     }
-
 
     @Override
     public void onColorChanged(int color) {
         surfaceView.setBackgroundColor(color);
-        Log.d(TAG, "Color = " + color);
+    }
+
+    @Override
+    public void onClick(View v) {
+        colorPicker.setColor(Color.BLUE);
     }
 }
